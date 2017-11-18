5c976321a2ed449573bbf837bcb7bfd53481d047
Philippe Charrière <ph.charriere@gmail.com>
2017-11-18 19:54:57 +0100

    deploy

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-PORT=9090 TOKEN="hello" API="hello" CREDENTIALS="hello" golo golo --classpath jars/*.jar --files imports/*.golo main.golo
+echo $1
+
+# PORT=9090 TOKEN="hello" API="hello" CREDENTIALS="hello" golo golo --classpath jars/*.jar --files imports/*.golo main.golo
